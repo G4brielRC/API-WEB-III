4e162deb8988c168f66e1320b90a424c6be414a6
G4brielRC <gabriel.crepaldi9@etec.sp.gov.br>
2025-08-29 22:39:12 -0300

    metodos com API

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controller\ProdutosControler;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+//rotas para visualizar os registros
+Route::get('/',function(){return response()->json(['Sucesso'=>true]);});
+Route::get('/produtos', [ProdutosController::class,'show']);
+Route::get('/produtos/{codigo}',[ProdutosController::class,'store']);
+
+//rota para alterar os registros 
+Route::post('/produtos',[ProdutosController::class,'update']);
+
+//rota para excluiir o registro por id/codigo
+Route::delete('/produtos/{id}', [ProdutosController::class,'destroy']);
+
